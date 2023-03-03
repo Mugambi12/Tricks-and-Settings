46b22af5b54fac07653fed22b9bf261f8145e7c4
mugambi12 <silasmungiria16@gmail.com>
2023-03-03 10:27:04 +0300

    initial commit

--- hangman game/hangman.py
+++ hangman game/hangman.py
@@ -1,5 +1,5 @@
 MAX_WORD_LENGTH = 10
-MAX_GUESSES = len(set(input(f"Enter a word for someone to guess (Max {MAX_WORD_LENGTH}): ").lower()))
+MAX_GUESSES = 3
 
 # Get the word to guess
 while True:
@@ -15,7 +15,7 @@ incorrect_guesses = 0
 hangman_correct = "😄"
 hangman_incorrect = "😔"
 
-while incorrect_guesses < MAX_GUESSES:
+while True:
     # Print out the partially guessed word
     word_display = ""
     for letter in word:
@@ -26,11 +26,16 @@ while incorrect_guesses < MAX_GUESSES:
     print(word_display)
 
     # Get the player's guess
-    guess = input("Guess a letter: ").lower()
+    guess = input("Guess a letter or the entire word: ").lower()
+
+    # Check if the guess is the entire word
+    if guess == word:
+        print("Congratulations, you have won!")
+        break
 
     # Check if the guess is a single letter or a space
     if len(guess) != 1 and guess != " ":
-        print("Please enter a single letter or a space.")
+        print("Please enter a single letter or the entire word.")
         continue
 
     # Check if the guess has already been made
@@ -44,13 +49,14 @@ while incorrect_guesses < MAX_GUESSES:
         print(f"Correct!\n\n{hangman_correct}\n")
     else:
         incorrect_guesses += 1
-        print(f"Incorrect!\n\n{hangman_incorrect}\n")
+        print(f"Incorrect! You have {MAX_GUESSES - incorrect_guesses} guesses left.\n\n{hangman_incorrect}\n")
 
     # Check if the player has won
     if all(letter in guessed_letters for letter in word):
-        print("Congratulations, you have won!")
+        print("Congratulations, you have won!{word}.")
         break
 
-# Check if the player has lost
-if incorrect_guesses == MAX_GUESSES:
-    print(f"Sorry, you have lost. The word was {word}")
+    # Check if the player has lost
+    if incorrect_guesses == MAX_GUESSES:
+        print(f"Sorry, you have lost. The word was {word}.")
+        break
